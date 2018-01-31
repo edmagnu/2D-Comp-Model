eb030f4289f61f66c051dccb7c859c962118eb8f
Eric Magnuson <edmagnu@gmail.com>
2018-01-30 21:36:22 -0500

    Combine meta and data into tidy DataFrame

--- computation/sim_analysis.py
+++ computation/sim_analysis.py
@@ -30,15 +30,56 @@ def atomic_units():
     return au
 
 
+def enfinal_plot(data):
+    """Given simulation results, plot the final orbital energy against launch
+    phase.
+    Returns None."""
+    au = atomic_units()
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    # mask only LRL and non-NaN
+    th_LRL = data["th_LRL"].unique()
+    labels = ["Uphill", "Downhill"]
+    for i in [0, 1]:
+        mask_u = data["th_LRL"] == th_LRL[i]
+        mask_nan = np.isnan(data["enfinal"])
+        mask = mask_u & np.logical_not(mask_nan)
+        # plot final energies
+        ax[i].plot(data[mask]["phi"], data[mask]["enfinal"]/au["GHz"], ".",
+                   color="C0", label=labels[i])
+        # plot NaN
+        mask = mask_u & mask_nan
+        ax[i].plot(data[mask]["phi"], data[mask]["phi"]*0, "X", color="C3",
+                   label="missing")
+        ax[i].set_ylabel(r"$ E_f = -\frac{1}{r} + \frac{1}{2}v^2$ (GHz)")
+        ax[i].grid(True)
+        ax[i].legend()
+    # label and tick axes
+    ax[1].set_xlabel(r"MW Launch Phase $\phi_0$ (rad.)")
+    ax[1].set_xticks(np.array([1/6, 4/6, 7/6, 10/6])*np.pi)
+    ax[1].set_xticklabels([r"$\pi/6$", r"$4\pi/6$", r"$7\pi/6$", r"$10\pi/6$"])
+    plt.tight_layout()
+    return None
+
+
 def main():
     """Read in results data file with metadata"""
+    au = atomic_units()
+    # specify file
     directory = ("C:\\Users\\edmag\\Documents\\Work\\" +
                  "2D-Comp-Model\\computation\\results")
     flist = os.listdir(directory)
-    meta = read_metadata(directory + "\\" + flist[0])
-    au = atomic_units()
-    print(float(meta["Emw"])/au["mVcm"])
-    return meta
+    fname = directory + "\\" + flist[0]
+    print(fname)
+    # load metadata and data
+    meta = read_metadata(fname)
+    data = pd.read_csv(fname, sep="\t", comment="#", index_col=False)
+    # add in metadata
+    data["Filename"] = pd.Series([fname]*len(data["phi"]), dtype=str)
+    data["E0"] = pd.Series([meta["E0"]]*len(data["phi"]), dtype=float)
+    data["Ep"] = pd.Series([meta["Ep"]]*len(data["phi"]), dtype=float)
+    data = data[["Filename", "E0", "Ep", "dL", "th_LRL", "phi", "enfinal"]]
+    # enfinal_plot(data)
+    return meta, data
 
 
-meta = main()
+meta, data = main()
