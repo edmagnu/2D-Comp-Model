f4e2402e001073597fe303338b1fc314a442d508
Eric Magnuson <edmagnu@gmail.com>
2018-03-05 19:02:58 -0500

    New .py for Simple 1D Model. Just AU so far.

---
+++ computation/Simple-1D-Model/Simple-1D.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 17:01:42 2018
+
+@author: labuser
+"""
+
+# A simple 1D model for the phase dependent ionization and recombination in
+# static fields
+
+def atomic_units():
+    """Return a dictionary of atomic units"""
+    au = {"GHz": 1.51983e-7, "mVcm": 1.94469e-13, "ns": 4.13414e7}
+    return au
